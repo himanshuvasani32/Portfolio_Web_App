fa94b483265348a340c6d7f0410f389149ddd69f
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-07-27 12:59:54 +0530

    Added 2 more columns to put the portfolio data

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.png')
+    st.image('images/photo.png', width=600)
 
 with col2:
     st.title('Ardit Sulce')
@@ -14,3 +15,20 @@ with col2:
 I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built using python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
