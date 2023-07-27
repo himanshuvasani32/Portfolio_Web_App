0949ee709dfb92cd85e8923c058f9bf91d60525d
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-07-27 16:13:53 +0530

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header('Contact Me')
+
+with st.form(key='email_forms'):
+    user_email = st.text_input('Enter your email:')
+    message = st.text_area('Enter your message:')
+    button = st.form_submit_button('Submit')
